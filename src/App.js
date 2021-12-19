568781345c1ca638851081e460ff1bc82ba01ce6
Anselem <anselemodims@gmail.com>
2021-12-19 09:47:55 +0100

    Feat: implemented the use of jsx in the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet.js';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Hector",
-      animal: "Dog",
-      breed: "Dobberman",
-    }),
-    React.createElement(Pet, {
-      name: "Hercules",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-    React.createElement(Pet, {
-      name: "Fluffy",
-      animal: "Cat",
-      breed: "Egyptian Cat",
-    }),
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+  return (
+    <div>
+      <h1></h1>
+      <Pet name='Hector' animal='Dog' breed='Dobberman' />
+      <Pet name='Hercules' animal='Dog' breed='German Shepherd' />
+      <Pet name='Fluffy' animal='Cat' breed='Egyptian Cat' />
+    </div>
+  )
+}
+ReactDOM.render(<App/>, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,12 @@
-import React from 'react';
-
 const Pet = (props) => {
   const { name, breed, animal } = props;
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
