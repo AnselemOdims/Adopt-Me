7b8c3ab49f788036ef9d5e728c24e978fb6106e4
Anselem <anselemodims@gmail.com>
2021-12-19 08:15:18 +0100

    Tool: setup eslint for linting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 const Pet = (props) => {
-  const {name, breed, animal} = props;
-	return React.createElement('div', {}, [
-		React.createElement('h2', {}, name),
-		React.createElement('h3', {}, animal),
-		React.createElement('h3', {}, breed),
-	]);
+  const { name, breed, animal } = props;
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h3", {}, animal),
+    React.createElement("h3", {}, breed),
+  ]);
 };
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me'),
-		React.createElement(Pet, {name: 'Hector', animal: 'Dog', breed: 'Dobberman'}),
-		React.createElement(Pet, {name: 'Hercules', animal: 'Dog', breed: 'German Shepherd'}),
-		React.createElement(Pet, {name: 'Fluffy', animal: 'Cat', breed: 'Egyptian Cat'}),
-	]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Hector",
+      animal: "Dog",
+      breed: "Dobberman",
+    }),
+    React.createElement(Pet, {
+      name: "Hercules",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
+    React.createElement(Pet, {
+      name: "Fluffy",
+      animal: "Cat",
+      breed: "Egyptian Cat",
+    }),
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
