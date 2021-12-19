2c2b3331f27f3138405dd7022c125886c797f524
Anselem <anselemodims@gmail.com>
2021-12-19 08:01:44 +0100

    Feat: created props for indevidual components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-const Pet = () => {
+const Pet = (props) => {
+  const {name, breed, animal} = props;
 	return React.createElement('div', {}, [
-		React.createElement('h2', {}, 'Hector'),
-		React.createElement('h3', {}, 'Dog'),
-		React.createElement('h3', {}, 'Dobberman'),
+		React.createElement('h2', {}, name),
+		React.createElement('h3', {}, animal),
+		React.createElement('h3', {}, breed),
 	]);
 };
 
 const App = () => {
 	return React.createElement('div', {}, [
 		React.createElement('h1', {}, 'Adopt Me'),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet),
+		React.createElement(Pet, {name: 'Hector', animal: 'Dog', breed: 'Dobberman'}),
+		React.createElement(Pet, {name: 'Hercules', animal: 'Dog', breed: 'German Shepherd'}),
+		React.createElement(Pet, {name: 'Fluffy', animal: 'Cat', breed: 'Egyptian Cat'}),
 	]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
