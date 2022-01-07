8e0e6f5fa227bb923560c7f6949292edc08a2e42
Anselem <anselemodims@gmail.com>
2022-01-07 13:34:23 +0100

    Feat: installed the react-router and imported into our file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import SearchParams from './SearchParams';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
   return (
---
+++ src/Details.js
@@ -0,0 +1,8 @@
+
+const Details = () => {
+  return (
+    <h2>I am the details page</h2>
+  )
+}
+
+export default Details;
